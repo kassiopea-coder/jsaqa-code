ef9fda80cc3f017eda6a153d6c0ce976c99a4b83
Nadezhda <7_nadezhda@mail.ru>
2023-09-16 21:03:15 +0400

    initial commit

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,18 +9,22 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+  }, 5000);
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", link => link.getAttribute('href') );
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 5000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -29,6 +32,30 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
-  });
+    expect(actual).toContain("Get started with Team")
+  }, 4000);
+});
+
+describe("Titles of other pages", () => {
+  
+  test("Should find the title on /features", async () => {
+    await page.goto("https://github.com/features");
+    const title = "div.application-main main div.p-responsive.container-xl.text-center.mt-12.mb-6 h1";
+    const actual = await page.$eval(title, (link) => link.textContent);
+    expect(actual).toContain("The tools you need to build what you want.");
+  }, 5000);
+
+  test("Should find the Contact sales button on /features/security", async () => {
+    await page.goto("https://github.com/features/security");
+    const button = "div.position-relative.z-1.container-xl.mx-auto.px-3.pt-6.py-md-12.height-full.d-flex.flex-column.flex-justify-center";
+    const actualButton = await page.$eval(button, (link) => link.textContent);
+    expect(actualButton).toContain("Contact sales");
+  }, 5000);
+
+  test("Should find the title on /enterprise", async () => {
+    await page.goto("https://github.com/enterprise");
+    const title = " div.col-9-max.position-relative.z-2.ml-lg-4.ml-xl-0 h1";
+    const actual = await page.$eval(title, (link) => link.textContent);
+    expect(actual).toContain("Build like the best");
+  }, 5000);
 });
